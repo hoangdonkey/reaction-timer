fbd2ff9bedd945f9580e9a8624815e394ff8cf43
Kazuma <hoang.donkey@gmail.com>
2021-12-31 16:19:17 +0700

    'reaction-time'

---
+++ src/components/Result.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="result">
+    <p>Reaction time: {{ score }} ms</p>
+    <h2>{{ rank }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+props:['score'],
+data() {
+    return {
+        rank: null
+    }
+},
+mounted() {
+    if (this.score < 250) {
+        this.rank = 'Holy Sh*t! Ninja Finger!'
+    }
+    else if (this.score > 250 && this.score < 500) {
+        this.rank = 'Hmm...Quite Good Reflexes'
+    }
+    else {
+        this.rank = 'Turtle!'
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
